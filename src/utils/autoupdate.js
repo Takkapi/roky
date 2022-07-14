858cd7358a1f4b06e839ffd852e07c606f834d04
Takkapi <72162635+Takkapi@users.noreply.github.com>
2022-07-14 03:29:51 -0700

    Add files via upload

--- src/utils/autoupdate.js
+++ src/utils/autoupdate.js
@@ -3,3 +3,9 @@ const {version} = require('../../package.json')
 
 let activeVersion = version;
 
+let latestVersion = fetch('https://raw.githubusercontent.com/Takkapi/roky/main/package.json').json();
+if(activeVersion !== latestVersion.version) {
+    console.log('You are running an outdated version of the bot and may miss some important patches/features! Please update as soon as possibe')
+}
+
+let commandFoldersList = fetch
